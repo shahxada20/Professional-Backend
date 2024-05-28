25d858db55e716c30cbba7d1e2c854714a512a6e
Shahzad <shahzadarain144@gmail.com>
2024-05-28 16:04:11 +0500

    added utils code

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
 export const DB_NAME = "chai_backend_db"
+// CORS - Cross Origin Resource Sharing
--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 // require('dotenv').config({path:'./env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+import { app } from "./app.js"
 
 dotenv.config({
     path: './env'
 })
 
 connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running on port: ${process.env.PORT}`)
+        })
+        app.on("error", () => {
+            console.log("Error: ", error)
+            throw error
+        })
+    })
+    .catch((err) => {
+        console.log("MONGODB connection failed !!!", err)
+    })
 /*
 import { DB_NAME } from "./constants";
 import express from "express";
@@ -16,11 +29,11 @@ const app = express();
 (async () => {
     try {
         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        
         app.on("error", () => {
             console.log("Error: ", error)
             throw error
         })
-
         app.listen(process.env.PORT, () => {
             console.log(`App is listening on port: ${process.env.PORT}`)
         })
